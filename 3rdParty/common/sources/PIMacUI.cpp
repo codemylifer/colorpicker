53c1dbdc87ae7d050f10e0bed19825df7a23372d
Frank Park <y.piao.us@ieee.org>
2016-04-08 00:16:05 -0400

    Initial Commit

---
+++ 3rdParty/common/sources/PIMacUI.cpp
@@ -0,0 +1,31 @@
+// ADOBE SYSTEMS INCORPORATED
+// Copyright  1993 - 2002 Adobe Systems Incorporated
+// All Rights Reserved
+//
+// NOTICE:  Adobe permits you to use, modify, and distribute this 
+// file in accordance with the terms of the Adobe license agreement
+// accompanying it.  If you have received this file from a source
+// other than Adobe, then your use, modification, or distribution
+// of it requires the prior written permission of Adobe.
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+//
+//	File:
+//		PIMacUI.cpp
+//
+//	Description:
+//		Macintosh only code for the UI elements
+//
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+//	Includes
+//-------------------------------------------------------------------------------
+#if __APPLE__
+#include "PIUI.h"
+
+void DoAbout(SPPluginRef plugin, int dialogID)
+{
+	DebugStr("\pNOT IMPLEMENTED void DoAbout(SPPluginRef plugin, int dialogID)");
+}
+#endif
+// end PIMacUI.cpp
